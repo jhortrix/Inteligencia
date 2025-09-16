4cb950385e72d29c4edc0eba693ea56a0d0c83e8
jhortrix <jhortrix11@gmail.com>
2025-09-15 19:31:33 -0500

    aparecen errores raros

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-if __name__ == '_main__':
 
+from flask import Flask, render_template
+
+if __name__ == '__main__':
 
 app = Flask(__name__)
 
@@ -7,7 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '_main__':
+if __name__ == '__main__':
     app.run(debug=True)
 
 
