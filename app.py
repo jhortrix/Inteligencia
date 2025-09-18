851466416dd12762fe60c602fde2ca507aa6cde0
jhortrix <jhortrix11@gmail.com>
2025-09-18 17:02:37 -0500

    Nuevos menus

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for
 
 app = Flask(__name__)
 
@@ -6,12 +6,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/mision")
+def mision():
+    return render_template('mision.html')
+
+@app.route("/vision")
+def vision():
+    return render_template('vision.html')
+
+
 @app.route('/recibir_mensaje', methods=['POST'])
 def recibir_mensaje():
     datos = request.json
     mensaje = datos['mensaje']
     print("Mensaje recibido:", mensaje)
     return "Flask recibió: " + mensaje
+
+
+@app.route('/obtener_datos')
+def obtener_datos():
+    datos = {'nombre': 'Juan', 'edad': 30}
+    return jsonify(datos)  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
